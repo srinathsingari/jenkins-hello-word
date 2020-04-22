4a1870c6f049a010706d336f63f2d097d22367a2
srinathsingari <srinath2392@gmail.com>
2020-04-22 15:56:22 +0900

    Update JavaTest.java

--- JavaTest.java
+++ JavaTest.java
@@ -2,6 +2,6 @@ public class JavaTest {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("....Cloned from Git And Compiled and ran by jenkins web ....");
+		System.out.println("....Cloned from Git And Compiled and ran by jenkins web hook ....");
 	}
 	}
